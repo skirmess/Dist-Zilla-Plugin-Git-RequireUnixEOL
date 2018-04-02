dcccb03a75abce40274793e3291cf110bed32100
Sven Kirmess <sven.kirmess@kzone.ch>
2018-04-02 18:16:16 +0200

    1.000

--- lib/Dist/Zilla/Plugin/Git/RequireUnixEOL.pm
+++ lib/Dist/Zilla/Plugin/Git/RequireUnixEOL.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.002';
+our $VERSION = '1.000';
 
 use Moose;
 
@@ -118,7 +118,7 @@ Dist::Zilla::Plugin::Git::RequireUnixEOL - Enforce the correct line endings in y
 
 =head1 VERSION
 
-Version 0.002
+Version 1.000
 
 =head1 SYNOPSIS
 
